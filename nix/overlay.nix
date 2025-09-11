2d804a0f016b2ccd016f1fe130cf9b6a74a4d4aa
Emil J. Tywoniak <emil@tywoniak.eu>
2025-09-12 00:29:38 +0200

    nix: bump

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -7,16 +7,20 @@ final: prev: rec {
     }).overrideAttrs
       (
         finalAttrs: previousAttrs: {
-          version = "0.55";
+          version = "0.57";
           src = prev.fetchFromGitHub {
             owner = "YosysHQ";
             repo = "yosys";
-            tag = "v${finalAttrs.version}";
-            hash = "sha256-GddNbAtH5SPm7KTa5kCm/vGq4xOczx+jCnOSQl55gUI=";
+            rev = "8cd6ee65605104017d29ef102ee1552dba0d192a";
+            hash = "sha256-4yO1dSryw1SbhM8F5bxQiXxdbDaShrFp96elC3VqpRo=";
             fetchSubmodules = true;
             leaveDotGit = true;
             inherit (previousAttrs.src) postFetch; # Preserve the postFetch script
           };
+          patches = [
+            (builtins.elemAt previousAttrs.patches 1)
+            (builtins.elemAt previousAttrs.patches 2)
+          ];
           doCheck = false;
           makeFlags = previousAttrs.makeFlags ++ [
             "ENABLE_ABC=0"
